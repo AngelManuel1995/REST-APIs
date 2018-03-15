8315577489d496e30dc248a7b32d69bdfbdf474b
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-03-14 19:18:51 -0500

    Connection to database

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,5 +1,11 @@
 const { MongoClient, ObjectID } = require('mongodb');
 
+//Podemos generar nuestros pripios ObjectID s de la siuiente manera.
+// let obj1 = new ObjectID()
+// let obj2 = new ObjectID()
+// console.log(obj1 + " -- " + obj2)
+
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', ( err, client ) => {
 	
   if( err ){
@@ -20,23 +26,31 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', ( err, client ) => {
 	// 	console.log(JSON.stringify(result.ops, undefined, 2))
 
 	// })
-	db.collection('Users').insertOne( {
-		name:'Angel Manuel Góez Giraldo',
-		age:23,
-		location:'South America'
-	}, 
-		( err, result ) => {
-			if( err ){
-				return console.log('Unable to insert into Todo', err);
-			}
-			console.log(JSON.stringify(result.ops, undefined, 2))
-	})
+	// db.collection('Users').insertOne( {
+	// 	name:'Camila',
+	// 	age:22,
+	// 	location:'Colombia'
+	// }, 
+	// 	( err, result ) => {
+	// 		if( err ){
+	// 			return console.log('Unable to insert into Todo', err);
+	// 		}
+	// 	//	result.ops: es un arreglo que trae todos los registros insertados.
+	// 		console.log(result.ops[0]._id.getTimestamp())
+	
+	// })
 
 	client.close();
 
 })
 /**
  * Documentación:
+ * 
+ * Primero que todo antes de correr nuestra aplicación lo que debemos hacer es iniciar la base de datos y lo haremos de 
+ * ls siguiente forma.
+ * Tenemos qu ebicar el archivo llamado mongod que está en la siguiente dirección. mongo/bin y entonces lo ejecutaremos
+ * de la siguiete manera ./mongo --dbpath ~/mongo-data donde le indicareos que un ruta donde quedará nuestra base de datos
+ * 
  * const MongoClient = require('mongodb'): Entonces primero que todos lo que debemos haces es crear la constante con la que 
  * importaremos los metodos y propiedades de mongodb.
  * Esta constante que creamos MongoCliente tiene un método callback que nos devuelve una referecia a la base de datos que 
