c6e98bac5b0dcf09d223a583fd0d232bc861aa67
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-03-20 09:54:35 -0500

    GET /todos ready

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ const { User } = require('./models/user');
 
 
 const app = express();
+//Middleware
 app.use(bodyParse.json());
+
+// POST /todos
 app.post('/todos', ( req, res ) => {
 	
 	let todo = new Todo({
@@ -22,6 +25,18 @@ app.post('/todos', ( req, res ) => {
 		res.status(400).send( err )
 	})
 })
+
+// GET /todos
+app.get('/todos', ( req, res ) => {
+
+	Todo.find().then( (todos) => {
+		res.send({todos})
+	}, (e) => {
+		res.status(400).send(e)
+	})
+
+})
+
 
 app.listen('3000',() => {
 	console.log('Started on 3000 port')
