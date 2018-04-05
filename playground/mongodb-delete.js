e3665807551093d2d3e3a9cacb6d1033f4563a58
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-04-04 19:06:26 -0500

    Delete actions with mongodb

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,42 +1,42 @@
 const { MongoClient, ObjectID } = require('mongodb')
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', ( err, client ) => {
-    if( err ){
-        return console.log('Unable to connect to mongodb')
-    }
-    const db = client.db('TodoApp');
-    
-    //deleteMany
-    // db.collection('Users').deleteMany({
-    //     age:26
-    // }).then( ( result ) => {
-    //     console.log(result)
-    // })
+	if( err ){
+		return console.log('Unable to connect to mongodb')
+	}
+	const db = client.db('TodoApp');
+	
+	//deleteMany
+	// db.collection('Users').deleteMany({
+	//     age:26
+	// }).then( ( result ) => {
+	//     console.log(result)
+	// })
 
 
-    //deleteOne
-    // db.collection('Users').deleteOne({location:'Philadelphia'}).then( (result) => {
-    //     console.log(result)
-    // })
+	//deleteOne
+	// db.collection('Users').deleteOne({location:'Philadelphia'}).then( (result) => {
+	//     console.log(result)
+	// })
 
-    //findOneAndDelete
-    // db.collection('Users').findOneAndDelete({
-    //     location:'Philadelphia'
-    // }).then( ( result ) => {
-    //     console.log( result )
-    // })
+	//findOneAndDelete
+	// db.collection('Users').findOneAndDelete({
+	//     location:'Philadelphia'
+	// }).then( ( result ) => {
+	//     console.log( result )
+	// })
 
-    db.collection('Users').deleteMany({
-        name:'Daniela'
-    }).then( ( result ) => {
-        console.log(result)
-    })
-    console.log('\n')
-    db.collection('Users').findOneAndDelete({
-        _id: new ObjectID('5aa94ae1ee2d3b17cd317080')
-    }).then( ( result ) => {
-        console.log(result)
-    })
+	db.collection('Users').deleteMany({
+		name:'Daniela'
+	}).then( ( result ) => {
+		console.log(result)
+	})
+	console.log('\n')
+	db.collection('Users').findOneAndDelete({
+		_id: new ObjectID('5aa94ae1ee2d3b17cd317080')
+	}).then( ( result ) => {
+		console.log(result)
+	})
 
 })
 //Podemos entonces usar metodos que nos son dados para eliminar registros de la base de datos
