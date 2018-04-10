e7e7c38465b7d888193c52f35945c46a5afcd830
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-04-09 20:16:54 -0500

    queries using mongoose

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -6,30 +6,31 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
+/*
+	// let id = '5ab11b203a650905d6a305be';
 
-// let id = '5ab11b203a650905d6a305be';
+	// if(!ObjectID.isValid(id)){
+	//     console.log('Id not valid')
+	// }
 
-// if(!ObjectID.isValid(id)){
-//     console.log('Id not valid')
-// }
+	// Todo.find({
+	//     _id:id
+	// }).then( (todos) => {
+	//     console.log('Todos', todos)
+	// })
 
-// Todo.find({
-//     _id:id
-// }).then( (todos) => {
-//     console.log('Todos', todos)
-// })
+	// Todo.findOne({
+	//     _id:id
+	// }).then( (todo) => {
+	//     console.log('Todo',todo)
+	// })
 
-// Todo.findOne({
-//     _id:id
-// }).then( (todo) => {
-//     console.log('Todo',todo)
-// })
-
-// Todo.findById(id).then((todo)=>{
-//     console.log('Todo By Id', todo)
-// }).catch( (e) => {
-//     console.log(e)
-// })
+	// Todo.findById(id).then((todo)=>{
+	//     console.log('Todo By Id', todo)
+	// }).catch( (e) => {
+	//     console.log(e)
+	// })
+*/
 
 let id = '5aadd9189d9ee2433993f923';
 User.findById(id).then(( user ) => {
