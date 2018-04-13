2d315cd48933ec45d982644ffc3855fa04ad9545
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-04-13 13:34:12 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParse = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -83,6 +84,30 @@ app.delete('/todos/:id', ( req, res ) => {
 
 // PATCH /todos/:id
 app.patch('/todos/:id', ( req, res ) => {
+
+	let id = req.params.id;
+	let body = _.pick(req.body, ['text', 'completed']);
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed){
+		body.completedAt = new Date().getTime();
+	}else{
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, { $set: body }, {new: true}).then( (todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch( (err) => {
+		res.status(400).send();
+	});
+	
 
 })
 
