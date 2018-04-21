4282950416119ba92b9b473dbdde2eb45a12e05b
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-04-21 14:42:58 -0500

    GET /users/me getting with token

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
-const _ = require('lodash');
-const express = require('express');
-const bodyParse = require('body-parser');
-const {ObjectID} = require('mongodb');
+const _ = require('lodash') 
+const express = require('express') 
+const bodyParse = require('body-parser') 
+const {ObjectID} = require('mongodb') 
 
 
 //Local imports
-const { mongoose } =  require('./db/mongoose');
-const { Todo } = require('./models/todo');
-const { User } = require('./models/user');
+const { mongoose } =  require('./db/mongoose') 
+const { Todo } = require('./models/todo') 
+const { User } = require('./models/user') 
+const { authenticate } = require('./middleware/authenticate')
 
 
-const app = express();
+const app = express() 
 //Middleware para que podamos retornar json
-app.use(bodyParse.json());
+app.use(bodyParse.json()) 
 
 // ******************* todos CRUD *******************
 // POST /todos
@@ -43,23 +44,23 @@ app.get('/todos', ( req, res ) => {
 // GET /todos/:id
 app.get('/todos/:id', ( req, res ) => {
 
-	let id = req.params.id;
+	let id = req.params.id 
 	
 	//Validaos que el id que nos envian sea válido
 	if( !ObjectID.isValid(id) ){
-		return res.status(404).send();
+		return res.status(404).send() 
 	}
 
 	//Buscamos el id usando findById del mongoose
 	Todo.findById(id).then( (todo) => {
 		//todo puede estar vacio, porque no hay registro con ese id
 		if( !todo ){
-			return res.status(404).send();
+			return res.status(404).send() 
 		}
 		//Retornamos el JSON del body
-		res.send({todo});
+		res.send({todo}) 
 	}).catch( (e) => {
-		res.status(400).send();
+		res.status(400).send() 
 	})
 	
 })
@@ -67,47 +68,47 @@ app.get('/todos/:id', ( req, res ) => {
 // DELETE /todos/:id
 app.delete('/todos/:id', ( req, res ) => {
 	
-	let id = req.params.id;
+	let id = req.params.id 
 	if( !ObjectID.isValid(id) ){
-		return res.status(404).send();
+		return res.status(404).send() 
 	}
 
 	Todo.findByIdAndRemove(id).then( (todo) => {
 		if(!todo){
-			return res.status(404).send();
+			return res.status(404).send() 
 		}
-		res.send({todo});
+		res.send({todo}) 
 
 	}).catch( (e) => {
-		res.status(400).send();
+		res.status(400).send() 
 	})
 })
 
 // PATCH /todos/:id
 app.patch('/todos/:id', ( req, res ) => {
 
-	let id = req.params.id;
-	let body = _.pick(req.body, ['text', 'completed']);
+	let id = req.params.id 
+	let body = _.pick(req.body, ['text', 'completed']) 
 
 	if(!ObjectID.isValid(id)){
-		return res.status(404).send();
+		return res.status(404).send() 
 	}
 
 	if(_.isBoolean(body.completed) && body.completed){
-		body.completedAt = new Date().getTime();
+		body.completedAt = new Date().getTime() 
 	}else{
-		body.completed = false;
-		body.completedAt = null;
+		body.completed = false 
+		body.completedAt = null 
 	}
 
 	Todo.findByIdAndUpdate(id, { $set: body }, {new: true}).then( (todo) => {
 		if(!todo){
-			return res.status(404).send();
+			return res.status(404).send() 
 		}
-		res.send({todo});
+		res.send({todo}) 
 	}).catch( (err) => {
-		res.status(400).send();
-	});
+		res.status(400).send() 
+	}) 
 	
 
 })
@@ -118,17 +119,21 @@ app.patch('/todos/:id', ( req, res ) => {
 // POST /users
 app.post('/users', ( req, res ) => {
 
-	let userParams = _.pick( req.body, ['email','password']);
-	let user = new User(userParams);
+	let userParams = _.pick( req.body, ['email','password'])
+	let user = new User(userParams)
 
 	user.save().then( () => {
-		return user.generateAuthToken();
+		return user.generateAuthToken()
 	}).then( ( token ) => {
-		res.header( 'x-auth', token ).send(user);
+		res.header( 'x-auth', token ).send(user)
 	}).catch( ( err ) => {
-		res.status(400).send(err);
+		res.status(400).send(err)
 	})
 
+})
+
+app.get( "/users/me", authenticate ,( req, res ) => {
+	res.send(req.user)
 })
 
 
